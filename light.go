d58917b8c4e64094f1a2dde81951e0dea3e501c0
li <lixiao.on@gmail.com>
2014-09-05 13:05:23 +0800

    add framework version
    add framework version

---
+++ light.go
@@ -0,0 +1,11 @@
+// Copyright 2014 li. All rights reserved.
+// Use of this source code is governed by a MIT/X11
+// license that can be found in the LICENSE file.
+
+package light
+
+// Version for current light framework.
+// Major.Minor.Revision
+const (
+	Version = "0.1.0"
+)
